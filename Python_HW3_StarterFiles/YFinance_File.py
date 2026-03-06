e06f424fa4a0745d2e2cbcb76c63317fdd58111a
David Romoff <74327064+DavidRomoff@users.noreply.github.com>
2026-03-06 08:49:15 -0500

    Update YFinance_File.py

--- Python_HW3_StarterFiles/YFinance_File.py
+++ Python_HW3_StarterFiles/YFinance_File.py
@@ -1,24 +1,6 @@
 import yfinance as yf
 import pandas as pd
 
-
-columns = pd.MultiIndex.from_tuples([
-    ('Close', 'AAPL'),
-    ('High', 'AAPL'),
-    ('Low', 'AAPL'),
-    ('Open', 'AAPL'),
-    ('Volume', 'AAPL')
-], names=['Metric', 'Ticker'])
-
-sample_data_rows = [
-    [150.00, 152.50, 149.00, 151.00, 10000000],
-    [151.50, 153.00, 150.50, 150.50, 11000000],
-    [153.00, 154.00, 152.00, 152.00, 9500000]
-]
-
-# Create a DataFrame
-SampleData = pd.DataFrame(sample_data_rows, columns=columns)
-
 def YahooData2returns(YahooData=SampleData,symbol='AAPL'):
     # Input:
     # YahooData = data from Yahoo Finance
